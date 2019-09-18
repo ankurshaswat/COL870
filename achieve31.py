51c85a4c2bcae7e76b0bac26f60923bd3e60976b
Shashwat Shivam <shivamshaswat@gmail.com>
2019-09-18 23:40:48 +0530

    Completed Simulator

--- achieve31.py
+++ achieve31.py
@@ -1,18 +1,90 @@
+"""
+COL870 A1 Code
+"""
+
 from numpy.random import choice, randint
 
 
+def print_card(player, sign, number):
+    """
+    Convert draws into proper prints for debugging
+    """
+
+    if player:
+        if sign == '+':
+            print("Player B"+str(number))
+        else:
+            print("Player R"+str(number))
+    else:
+        if sign == '+':
+            print("Dealer B"+str(number))
+        else:
+            print("Dealer R"+str(number))
+
+
+def score_state(state_to_score):
+    """
+    Score states for player and dealer
+    """
+    player_total = state_to_score['player_total']
+    player_trumps = state_to_score['player_trump1'] + \
+        state_to_score['player_trump2'] + state_to_score['player_trump3']
+
+    player_score = 0
+    for i in range(player_trumps, -1, -1):
+        player_score = player_total + i * 10
+        if player_score <= 31:
+            break
+
+    dealer_total = state_to_score['dealer_total']
+    dealer_trumps = state_to_score['dealer_trump1'] + \
+        state_to_score['dealer_trump2'] + state_to_score['dealer_trump3']
+
+    dealer_score = 0
+    for i in range(dealer_trumps, -1, -1):
+        dealer_score = dealer_total + i * 10
+        if dealer_score <= 31:
+            break
+
+    return player_score, dealer_score
+
+
+def generate_random_b_r():
+    """
+    Generate Random Card type
+    """
+    draw = choice(['+', '-'], 1, p=[2/3, 1/3])
+    return draw
+
+
+def generate_random_number():
+    """
+    Generate Random Next Card Number
+    """
+    draw = randint(low=1, high=11)
+    return draw
+
+
 class Simulator():
+    """
+    Class to simulate Achieve31 Games
+    """
+
     def __init__(self):
         self.initialize_enc_decoder()
-        self.reset()
+        self.state = {}
 
     def initialize_enc_decoder(self):
+        """
+        Initialize Encoder and Decoder for state compression
+        """
+
         state_num = 3
         self.encoder = {}
         self.decoder = {}
 
-        for s1 in range(-30, 32):
-            for s2 in range(-30, 32):
+        for player_total in range(-30, 32):
+            for dealer_total in range(-30, 32):
                 for p_t1 in range(0, 2):
                     for p_t2 in range(0, 2):
                         for p_t3 in range(0, 2):
@@ -20,19 +92,19 @@ class Simulator():
                                 for d_t2 in range(0, 2):
                                     for d_t3 in range(0, 2):
                                         constructed_state = {
-                                            'player_total': s1,
+                                            'player_total': player_total,
                                             'player_trump1': p_t1,
                                             'player_trump2': p_t2,
                                             'player_trump3': p_t3,
-                                            'dealer_total': s2,
+                                            'dealer_total': dealer_total,
                                             'dealer_trump1': d_t1,
                                             'dealer_trump2': d_t2,
                                             'dealer_trump3': d_t3,
                                         }
-                                        player_score, dealer_score = self.score_state(
+                                        player_score, dealer_score = score_state(
                                             constructed_state)
 
-                                        if not (player_score >= 0 and player_score <= 31 and dealer_score >= 0 and dealer_score <= 31):
+                                        if not (0 <= player_score <= 31 and 0 <= dealer_score <= 31):
                                             continue
 
                                         self.encoder[frozenset(
@@ -42,68 +114,68 @@ class Simulator():
 
         print('total_states = ', state_num)
 
-    def encode(self, type):
-        if (type == 'bust'):
+    def encode(self, type_state=''):
+        """
+        Encode input state
+        """
+        if type_state == 'lose':
             return 0
-        elif (type == 'win'):
+        if type_state == 'win':
             return 1
-        elif (type == 'lose'):
+        if type_state == 'draw':
             return 2
-        else:
-            return self.encoder[frozenset(self.state.items())]
 
-    def decode(self,state_num):
-        if(state_num == 0):
-            return 'bust'
-        elif state_num == 1:
-            return 'win'
-        elif state_num == 2:
+        return self.encoder[frozenset(self.state.items())]
+
+    def decode(self, state_num):
+        """
+        Decode input state
+        """
+        if state_num == 0:
             return 'lose'
-        else:
-            return self.decoder[state_num]
+        if state_num == 1:
+            return 'win'
+        if state_num == 2:
+            return 'draw'
 
-    def score_state(self, state_to_score):
-        player_total = state_to_score['player_total']
-        player_trumps = state_to_score['player_trump1'] + \
-            state_to_score['player_trump2'] + state_to_score['player_trump3']
+        return self.decoder[state_num]
 
-        player_score = 0
-        for i in range(player_trumps, -1, -1):
-            player_score = player_total + i * 10
-            if(player_score <= 31):
-                break
+    def check_and_return_next_state(self, state, end_game=False):
+        """
+        Check the scores and continue game if possible
+        """
+        player_score, dealer_score = score_state(state)
 
-        dealer_total = state_to_score['dealer_total']
-        dealer_trumps = state_to_score['dealer_trump1'] + \
-            state_to_score['dealer_trump2'] + state_to_score['dealer_trump3']
+        done = False
 
-        dealer_score = 0
-        for i in range(dealer_trumps, -1, -1):
-            dealer_score = dealer_total + i * 10
-            if(dealer_score <= 31):
-                break
-
-        return player_score, dealer_score
-
-    def generate_random_b_r(self):
-        draw = choice(['+', '-'], 1, p=[2/3, 1/3])
-        return draw
-
-    def generate_random_number(self):
-        draw = randint(low=1, high=10)
-        return draw
-
-    def check_and_return_next_state(self,state):
-        player_score, dealer_score = self.score_state(state)
+        if (player_score < 0 or player_score > 31) and (dealer_score < 0 or dealer_score > 31):
+            next_state, reward, done = self.encode('draw'), 0, True
 
         if player_score < 0 or player_score > 31:
-            return self.encode('bust'), True
-        elif dealer_score < 0 or dealer_score > 31:
-            return self.encode('win'), True
-        else:
-            return self.encode(''), False
+            next_state, reward, done = self.encode('lose'), -1, True
+
+        if dealer_score < 0 or dealer_score > 31:
+            next_state, reward, done = self.encode('win'), 1, True
+
+        if end_game and not done:
+            if player_score > dealer_score:
+                next_state, reward, done = self.encode('win'), 1, True
+
+            if player_score == dealer_score:
+                next_state, reward, done = self.encode('draw'), 0, True
+
+            if player_score < dealer_score:
+                next_state, reward, done = self.encode('lose'), -1, True
+
+        if not done:
+            next_state, reward, done = self.encode(), 0, False
+
+        return next_state, reward, done
 
     def reset(self):
+        """
+        Reset State and start new game
+        """
         self.state = {
             'player_total': 0,
             'player_trump1': 0,
@@ -115,87 +187,98 @@ class Simulator():
             'dealer_trump3': 0,
         }
 
-        card_sign = self.generate_random_b_r()
-        card_num = self.generate_random_number()
+        card_num = generate_random_number()
 
-        if(card_sign == '+'):
-            self.state['player_total'] = card_num
-            if(card_num == 1):
-                self.state['player_trump1'] = 1
-            elif (card_num == 2):
-                self.state['player_trump2'] = 1
-            elif (card_num == 3):
-                self.state['player_trump3'] = 1
-        else:
-            self.state['player_total'] = -1 * card_num
+        self.state['player_total'] = card_num
+        if card_num == 1:
+            self.state['player_trump1'] = 1
+        elif card_num == 2:
+            self.state['player_trump2'] = 1
+        elif card_num == 3:
+            self.state['player_trump3'] = 1
 
-        card_sign = self.generate_random_b_r()
-        card_num = self.generate_random_number()
+        print_card(True, '+', card_num)
 
-        if(card_sign == '+'):
-            self.state['dealer_total'] = card_num
-            if(card_num == 1):
-                self.state['dealer_trump1'] = 1
-            elif (card_num == 2):
-                self.state['dealer_trump2'] = 1
-            elif (card_num == 3):
-                self.state['dealer_trump3'] = 1
-        else:
-            self.state['dealer_total'] = -1 * card_num
+        card_num = generate_random_number()
 
-        init_state, _ = self.check_and_return_next_state(self.state)
-        return init_state
+        self.state['dealer_total'] = card_num
+        if card_num == 1:
+            self.state['dealer_trump1'] = 1
+        elif card_num == 2:
+            self.state['dealer_trump2'] = 1
+        elif card_num == 3:
+            self.state['dealer_trump3'] = 1
+
+        print_card(False, '+', card_num)
+
+        init_state_loc, _, _ = self.check_and_return_next_state(self.state)
+        return init_state_loc
 
     def step(self, action):
-        reward = 0
-        done = False
-
+        """
+        Take next step according to action
+        """
         if action == 'hit':
-            card_sign = self.generate_random_b_r()
-            card_num = self.generate_random_number()
+            card_sign = generate_random_b_r()
+            card_num = generate_random_number()
 
-            if(card_sign == '+'):
+            if card_sign == '+':
                 self.state['player_total'] += card_num
-                if(card_num == 1):
+                if card_num == 1:
                     self.state['player_trump1'] = 1
-                elif (card_num == 2):
+                elif card_num == 2:
                     self.state['player_trump2'] = 1
-                elif (card_num == 3):
+                elif card_num == 3:
                     self.state['player_trump3'] = 1
             else:
                 self.state['player_total'] -= card_num
 
-            next_state , done = self.check_and_return_next_state(self.state)
+            print_card(True, card_sign, card_num)
 
         else:
             while True:
-                card_sign = self.generate_random_b_r()
-                card_num = self.generate_random_number()
+                card_sign = generate_random_b_r()
+                card_num = generate_random_number()
 
-                if(card_sign == '+'):
-                    self.state['dealer_total'] = card_num
-                    if(card_num == 1):
+                if card_sign == '+':
+                    self.state['dealer_total'] += card_num
+                    if card_num == 1:
                         self.state['dealer_trump1'] = 1
-                    elif (card_num == 2):
+                    elif card_num == 2:
                         self.state['dealer_trump2'] = 1
-                    elif (card_num == 3):
+                    elif card_num == 3:
                         self.state['dealer_trump3'] = 1
                 else:
-                    self.state['dealer_total'] = -1 * card_num
+                    self.state['dealer_total'] -= card_num
 
-                _, dealer_score = self.score_state(self.state)
+                print_card(False, card_sign, card_num)
 
-                if(dealer_score < 0 or dealer_score >= 25):
+                player_score, dealer_score = score_state(self.state)
+
+                print('Projected Scores', player_score, dealer_score)
+
+                if(dealer_score < 0 or dealer_score > 31) or dealer_score >= 25:
                     break
-            # Complete dealer action sequencess
-            done = 1
+
+        next_state, reward, done = self.check_and_return_next_state(
+            self.state, action != 'hit')
+
         return next_state, reward, done
 
 
-sim = Simulator()
-init_state = sim.reset()
-print(sim.decode(init_state))
-done = False
-while not done:
-    next_state, reward, done = sim.step('hit')
-    print(sim.decode(next_state), reward, done)
+SIM = Simulator()
+INIT_STATE = SIM.reset()
+DECODED_STATE = SIM.decode(INIT_STATE)
+print(DECODED_STATE)
+print('Projected Scores', score_state(DECODED_STATE))
+DONE = False
+while not DONE:
+    PLAYER_SCORE, _ = score_state(DECODED_STATE)
+    if PLAYER_SCORE < 25:
+        NEXT_STATE, REWARD, DONE = SIM.step('hit')
+    else:
+        NEXT_STATE, REWARD, DONE = SIM.step('stick')
+    DECODED_STATE = SIM.decode(NEXT_STATE)
+    print(DECODED_STATE, REWARD, DONE)
+    if not DONE:
+        print('Projected Scores', score_state(DECODED_STATE))
