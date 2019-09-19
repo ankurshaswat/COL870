443a13b6d640161adaf5aa269c5635e6bc64f57a
Shashwat Shivam <shivamshaswat@gmail.com>
2019-09-19 15:17:45 +0530

    Refactor and backup

--- achieve31.py
+++ achieve31.py
@@ -30,10 +30,10 @@ def score_state(state_to_score):
     player_trumps = state_to_score['player_trump1'] + \
         state_to_score['player_trump2'] + state_to_score['player_trump3']
 
-    player_score = 0
-    for i in range(player_trumps, -1, -1):
-        player_score = player_total + i * 10
-        if player_score <= 31:
+    player_score_loc = 0
+    for num_trumps in range(player_trumps, -1, -1):
+        player_score_loc = player_total + num_trumps * 10
+        if player_score_loc <= 31:
             break
 
     dealer_total = state_to_score['dealer_total']
@@ -41,12 +41,12 @@ def score_state(state_to_score):
         state_to_score['dealer_trump2'] + state_to_score['dealer_trump3']
 
     dealer_score = 0
-    for i in range(dealer_trumps, -1, -1):
-        dealer_score = dealer_total + i * 10
+    for num_trumps in range(dealer_trumps, -1, -1):
+        dealer_score = dealer_total + num_trumps * 10
         if dealer_score <= 31:
             break
 
-    return player_score, dealer_score
+    return player_score_loc, dealer_score
 
 
 def generate_random_b_r():
@@ -101,10 +101,10 @@ class Simulator():
                                             'dealer_trump2': d_t2,
                                             'dealer_trump3': d_t3,
                                         }
-                                        player_score, dealer_score = score_state(
+                                        player_score_loc, dealer_score = score_state(
                                             constructed_state)
 
-                                        if not (0 <= player_score <= 31 and 0 <= dealer_score <= 31):
+                                        if not (0 <= player_score_loc <= 31 and 0 <= dealer_score <= 31):
                                             continue
 
                                         self.encoder[frozenset(
@@ -144,33 +144,37 @@ class Simulator():
         """
         Check the scores and continue game if possible
         """
-        player_score, dealer_score = score_state(state)
+        player_score_loc, dealer_score = score_state(state)
 
-        done = False
+        done_loc = False
 
-        if (player_score < 0 or player_score > 31) and (dealer_score < 0 or dealer_score > 31):
-            next_state, reward, done = self.encode('draw'), 0, True
+        if not 0 <= player_score_loc <= 31 and not 0 <= dealer_score <= 31:
+            next_state_loc, reward_loc, done_loc = self.encode('draw'), 0, True
 
-        if player_score < 0 or player_score > 31:
-            next_state, reward, done = self.encode('lose'), -1, True
+        if player_score_loc < 0 or player_score_loc > 31:
+            next_state_loc, reward_loc, done_loc = self.encode(
+                'lose'), -1, True
 
         if dealer_score < 0 or dealer_score > 31:
-            next_state, reward, done = self.encode('win'), 1, True
+            next_state_loc, reward_loc, done_loc = self.encode('win'), 1, True
 
-        if end_game and not done:
-            if player_score > dealer_score:
-                next_state, reward, done = self.encode('win'), 1, True
+        if end_game and not done_loc:
+            if player_score_loc > dealer_score:
+                next_state_loc, reward_loc, done_loc = self.encode(
+                    'win'), 1, True
 
-            if player_score == dealer_score:
-                next_state, reward, done = self.encode('draw'), 0, True
+            if player_score_loc == dealer_score:
+                next_state_loc, reward_loc, done_loc = self.encode(
+                    'draw'), 0, True
 
-            if player_score < dealer_score:
-                next_state, reward, done = self.encode('lose'), -1, True
+            if player_score_loc < dealer_score:
+                next_state_loc, reward_loc, done_loc = self.encode(
+                    'lose'), -1, True
 
-        if not done:
-            next_state, reward, done = self.encode(), 0, False
+        if not done_loc:
+            next_state_loc, reward_loc, done_loc = self.encode(), 0, False
 
-        return next_state, reward, done
+        return next_state_loc, reward_loc, done_loc
 
     def reset(self):
         """
@@ -214,11 +218,11 @@ class Simulator():
         init_state_loc, _, _ = self.check_and_return_next_state(self.state)
         return init_state_loc
 
-    def step(self, action):
+    def step(self, action_taken):
         """
         Take next step according to action
         """
-        if action == 'hit':
+        if action_taken == 'hit':
             card_sign = generate_random_b_r()
             card_num = generate_random_number()
 
@@ -253,49 +257,50 @@ class Simulator():
 
                 # print_card(False, card_sign, card_num)
 
-                player_score, dealer_score = score_state(self.state)
+                _, dealer_score = score_state(self.state)
 
                 # print('Projected Scores', player_score, dealer_score)
 
                 if(dealer_score < 0 or dealer_score > 31) or dealer_score >= 25:
                     break
 
-        next_state, reward, done = self.check_and_return_next_state(
-            self.state, action != 'hit')
+        next_state_loc, reward_loc, done_loc = self.check_and_return_next_state(
+            self.state, action_taken != 'hit')
 
-        return next_state, reward, done
+        return next_state_loc, reward_loc, done_loc
 
 
-episodes = []
-SIM = Simulator()
+if __name__ == "__main__":
+    EPISODES = []
+    SIM = Simulator()
 
-for i in range(10):
-    episode = []
+    for i in range(10):
+        episode = []
 
-    INIT_STATE = SIM.reset()
-    episode.append(INIT_STATE)
-    DECODED_STATE = SIM.decode(INIT_STATE)
-    # print(DECODED_STATE)
-    # print('Projected Scores', score_state(DECODED_STATE))
-    DONE = False
-    ACTION = 'none'
-    while not DONE:
-        PLAYER_SCORE, _ = score_state(DECODED_STATE)
-
-        if PLAYER_SCORE < 25:
-            ACTION = 'hit'
-            NEXT_STATE, REWARD, DONE = SIM.step('hit')
-        else:
-            ACTION = 'stick'
-            NEXT_STATE, REWARD, DONE = SIM.step('stick')
-        DECODED_STATE = SIM.decode(NEXT_STATE)
-        episode.append(ACTION)
-        episode.append(REWARD)
-        episode.append(NEXT_STATE)
-        # print(DECODED_STATE, REWARD, DONE)
-        # if not DONE:
+        init_state = SIM.reset()
+        episode.append(init_state)
+        decoded_state = SIM.decode(init_state)
+        # print(DECODED_STATE)
         # print('Projected Scores', score_state(DECODED_STATE))
-    print((len(episode)-1) / 3)
-    episodes.append(episode)
+        done = False
+        action = 'none'
+        while not done:
+            player_score, _ = score_state(decoded_state)
 
-print(episodes)
+            if player_score < 25:
+                action = 'hit'
+                next_state, reward, done = SIM.step('hit')
+            else:
+                action = 'stick'
+                next_state, reward, done = SIM.step('stick')
+            decoded_state = SIM.decode(next_state)
+            episode.append(action)
+            episode.append(reward)
+            episode.append(next_state)
+            # print(DECODED_STATE, REWARD, DONE)
+            # if not DONE:
+            # print('Projected Scores', score_state(DECODED_STATE))
+        print((len(episode)-1) / 3)
+        EPISODES.append(episode)
+
+    print(EPISODES)
